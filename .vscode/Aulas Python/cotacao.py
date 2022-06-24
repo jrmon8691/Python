92209ba259ef3965164426d85882dd925cf63c5c
jrmon8691 <you@example.com>
2022-06-24 10:38:41 -0300

    master

--- .vscode/Aulas Python/cotacao.py
+++ .vscode/Aulas Python/cotacao.py
@@ -1,13 +1,18 @@
-import requests
-from datetime import datetime
+import requests as rq
+import time
+import datetime as dt
 
-requisicao = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
+#conversao de data e hora
+d = dt.datetime.now()
+data = f'{d.day}/{d.month}/{d.year}'
+hora = f'{d.hour}:{d.minute}:{d.second}'
 
+requisicao = rq.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
 requisicao_dic = requisicao.json()
 
 cotacao_dolar = requisicao_dic['USDBRL']['bid']
 cotacao_euro  = requisicao_dic['EURBRL']['bid']
 cotacao_btc   = requisicao_dic['BTCBRL']['bid']
-
-print(f'\n* Cotacoes de Moedas *\n\nDolar\t=> {cotacao_dolar}\n\
-Euro\t=> {cotacao_euro}\nBTC\t=> {cotacao_btc}\n\nAtualizado:{datetime.now()}')
+    
+print(f'\n* Cotação de Moedas *\n\nDolar\t=> R${cotacao_dolar}\n\
+Euro\t=> R${cotacao_euro}\nBitcoin\t=> R${cotacao_btc}\n\nUpdated:\n{data} | {hora}')
